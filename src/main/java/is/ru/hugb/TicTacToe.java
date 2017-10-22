4541e51042195e0953f61c5e2b0d8eaf111a4d2e
Einar Helgi Guðmundsson <einar.h.gudm@gmail.com>
2017-10-22 21:42:11 +0000

    Added Heroku support

--- src/main/java/is/ru/hugb/TicTacToe.java
+++ src/main/java/is/ru/hugb/TicTacToe.java
@@ -4,11 +4,20 @@ import static spark.Spark.*;
 
 public class TicTacToe {
 	public static void main(String[] args) {
+		port(getHerokuPort());
 		get("/", (req, res) -> sayHi());
 	}
 	
 	public static String sayHi() {
 		return "Hi!";
 	}
+	
+	static int getHerokuPort() {
+        ProcessBuilder psb = new ProcessBuilder();
+		if (psb.environment().get("PORT") != null) {
+			return Integer.parseInt(psb.environment().get("PORT"));
+		}
+		return 4567;
+    }
 }
 			
