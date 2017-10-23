e0d1cddd07eb3b7c3146b419a62bddb8a189c4c7
vilhjalmur12 <vilhjalmur12@ru.is>
2017-10-23 16:31:03 +0200

    Fært frá logic yfir í UI klasa, unit test byrjað

---
+++ src/main/java/is/ru/hugb/UI/WebUI.java
@@ -0,0 +1,18 @@
+package is.ru.hugb;
+
+import static spark.Spark.*;
+
+public class TicTacToe {
+	public static void main(String[] args) {
+		get("/", (req, res) -> sayHi());
+        get("/bye", (req, res) -> sayBye());
+	}
+	
+	public static String sayHi() {
+		return "Hi!";
+	}
+    
+    public static String sayBye() {
+        return "Bye!";
+    }
+}
