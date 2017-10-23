3daffcbadc3b42c424d5db631f0ff6891508c9b6
vilhjalmur12 <vilhjalmur12@ru.is>
2017-10-23 16:16:03 +0200

    tictactoe.java

--- src/main/java/is/ru/hugb/TicTacToe.java
+++ src/main/java/is/ru/hugb/TicTacToe.java
@@ -5,10 +5,15 @@ import static spark.Spark.*;
 public class TicTacToe {
 	public static void main(String[] args) {
 		get("/", (req, res) -> sayHi());
+        get("/bye", (req, res) -> sayBye());
 	}
 	
 	public static String sayHi() {
 		return "Hi!";
 	}
+    
+    public static String sayBye() {
+        return "Bye!";
+    }
 }
 			
