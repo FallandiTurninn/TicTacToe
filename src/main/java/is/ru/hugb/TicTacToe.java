2b4e4739da8e3398b204d9847d8421af347846f3
Alexander Jósep <alexanderjb16@ru.is>
2017-10-23 19:21:38 +0000

    Started on game logic

--- src/main/java/is/ru/hugb/TicTacToe.java
+++ src/main/java/is/ru/hugb/TicTacToe.java
@@ -1,28 +1,24 @@
 package is.ru.hugb;
 
-import static spark.Spark.*;
+public class TicTacToe implements Game {
 
-public class TicTacToe {
-	public static void main(String[] args) {
-		port(getHerokuPort());
-		get("/", (req, res) -> sayHi());
-        get("/bye", (req, res) -> sayBye());
+	private byte[][] data;
+
+	@Override
+	public void setup() {
+		data = new byte[3][3];
 	}
-	
-	static int getHerokuPort() {
-        ProcessBuilder psb = new ProcessBuilder();
-		if (psb.environment().get("PORT") != null) {
-			return Integer.parseInt(psb.environment().get("PORT"));
-		}
-		return 4567;
-    }
-	
-	public static String sayHi() {
-		return "Hi!";
+
+	@Override
+	public int loop() {
+		// 0 continue
+		// -1 exit
+		return 0;
 	}
-    
-    public static String sayBye() {
-        return "Bye!";
-    }
+
+	public TicTacToe() {
+		
+	}
+
 }
 			
--- src/test/java/is/ru/hugb/TicTacToe/TicTacToeTest.java
+++ src/test/java/is/ru/hugb/TicTacToe/TicTacToeTest.java
@@ -7,11 +7,18 @@ import org.junit.Test;
 public class TicTacToeTest {
 
     @Test
+    public void loopTest() {
+        TicTacToe game = new TicTacToe();
+        assertEquals(0, game.loop());
+    }
+
+    /*
+    @Test
     public void sayHiTest() {
         TicTacToe game = new TicTacToe();
         assertEquals("Hi!", game.sayHi());
     }
-    /*
+    
     @Test
     public void startsGame() {
         
