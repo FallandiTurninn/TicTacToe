c3e066e8ed5a2ae0e722559bcb4d04a89aab82fc
vilhjalmur12 <vilhjalmur12@ru.is>
2017-10-25 22:09:23 +0200

    unit test byrjað fyrir UI

---
+++ src/test/java/is/ru/hugb/TicTacToe/WebUITest.java
@@ -0,0 +1,22 @@
+/*
+package is.ru.hugb;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class WebUITest {
+
+    private WebUI UI;
+    
+    // Test sendir request sem post hvernig mode á að spila
+    
+    @Test
+    public void gameModeDataTest() {
+        
+        //single player mode
+        assertEquals(0, game.loop());
+    }
+    
+}
+*/
