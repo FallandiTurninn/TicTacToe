b827b5115c28019b6c654b975cc3b986471a1f0f
Arnar <addi1204@gmail.com>
2017-10-28 19:32:00 +0000

    Testing travis-ci Xvfb e2e-test

--- src/selenium/java/is/ru/hugb/SeleniumTestWrapper.java
+++ src/selenium/java/is/ru/hugb/SeleniumTestWrapper.java
@@ -2,19 +2,26 @@ package is.ru.hugb.WebUI;
 
 import java.util.concurrent.TimeUnit;
 
+
+//import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
 public abstract class SeleniumTestWrapper {
+  ChromeOptions options = new ChromeOptions()
+  //options.addExtensions(new File("/path/to/extension.crx"))
+  options.setBinary(new File("/home/travis/"));
+
+
   static ChromeDriver driver;
   static String baseUrl;
   static String port;
 
   @BeforeClass
   public static void openBrowser() {
-    driver = new ChromeDriver();
+    driver = new ChromeDriver(options);
     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
     port = System.getenv("PORT");
