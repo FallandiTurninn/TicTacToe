32c0a4210e3a2da106740cde3c329b10dff6291d
Travis CI <travis@travis-ci.org>
2017-10-28 20:03:13 +0000

    made some changes to the script

--- deployToPages.sh
+++ deployToPages.sh
@@ -1,11 +1,13 @@
-#!/bash
+#!/bin/bash
+set -e
+echo "starting build"
 git config --global user.name "Travis CI"
 git config --global user.email "travis@travis-ci.org"
-git checkout -b --orphan temp  # creates a new empty branch
+git checkout --orphan temp  # creates a new empty branch
 git fetch origin master  # gets all the info from our main branch
 git checkout origin/master -- docs # gets only the docs folder
 git add docs
 git commit -m "sending docs to FallandiTurninn.github.io"  
 git remote add pages git@github.com:FallandiTurninn/FallandiTurninn.github.io.git  # makes a remote to out gitpages site
-git push --quie pages temp:master  #pushes to our gitpages site
+git push --quiet pages temp:master  #pushes to our gitpages site
 # should go to the main branch and kill the temp branch
