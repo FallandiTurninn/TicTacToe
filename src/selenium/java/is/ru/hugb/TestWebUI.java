738ba910f968b8443fc12a3bb80609517944f3d9
Einar Helgi Guðmundsson <einar.h.gudm@gmail.com>
2017-10-29 16:00:31 +0000

    added selenium tests

--- src/selenium/java/is/ru/hugb/TestWebUI.java
+++ src/selenium/java/is/ru/hugb/TestWebUI.java
@@ -14,33 +14,71 @@ public class TestWebUI extends SeleniumTestWrapper {
     assertEquals("Tic Tac Toe", driver.getTitle());
 
   }
-
-  @Test
-  public void testPickMode() throws Exception {
-    driver.get(baseUrl);
-    /* Remove Thread.sleep... */
-    Thread.sleep(1000);
-    assertEquals("1", "1");
-    Thread.sleep(3500);
-    /* ... finish test! */
-  }
   
   @Test
   public void testXWin() throws Exception {
     driver.get(baseUrl);
     /* Remove Thread.sleep... */
-    Thread.sleep(1000);
+    Thread.sleep(2000);
     driver.findElement(By.id("0")).click();
-	Thread.sleep(500);
+	Thread.sleep(1000);
     driver.findElement(By.id("3")).click();
-	Thread.sleep(500);
+	Thread.sleep(1000);
     driver.findElement(By.id("1")).click();
-	Thread.sleep(500);
+	Thread.sleep(1000);
     driver.findElement(By.id("4")).click();
-	Thread.sleep(500);
+	Thread.sleep(1000);
     driver.findElement(By.id("2")).click();
-	Thread.sleep(500);
+	Thread.sleep(2000);
 	assertEquals("X is the WINNER!", driver.findElement(By.id("gameInfo")).getAttribute("innerHTML"));
+    /* ... finish test! */
+  }
+
+  @Test
+  public void testOWin() throws Exception {
+    driver.get(baseUrl);
+    /* Remove Thread.sleep... */
+    Thread.sleep(2000);
+    driver.findElement(By.id("0")).click();
+	Thread.sleep(1000);
+    driver.findElement(By.id("3")).click();
+	Thread.sleep(1000);
+    driver.findElement(By.id("1")).click();
+	Thread.sleep(1000);
+    driver.findElement(By.id("4")).click();
+	Thread.sleep(1000);
+    driver.findElement(By.id("6")).click();
+	Thread.sleep(1000);
+	driver.findElement(By.id("5")).click();
+	Thread.sleep(2000);
+	assertEquals("O is the WINNER!", driver.findElement(By.id("gameInfo")).getAttribute("innerHTML"));
+    /* ... finish test! */
+  }
+  
+  @Test
+  public void testTie() throws Exception {
+    driver.get(baseUrl);
+    /* Remove Thread.sleep... */
+    Thread.sleep(2000);
+    driver.findElement(By.id("0")).click();
+	Thread.sleep(1000);
+    driver.findElement(By.id("1")).click();
+	Thread.sleep(1000);
+    driver.findElement(By.id("3")).click();
+	Thread.sleep(1000);
+    driver.findElement(By.id("4")).click();
+	Thread.sleep(1000);
+    driver.findElement(By.id("7")).click();
+	Thread.sleep(1000);
+	driver.findElement(By.id("6")).click();
+	Thread.sleep(1000);
+    driver.findElement(By.id("2")).click();
+	Thread.sleep(1000);
+    driver.findElement(By.id("5")).click();
+	Thread.sleep(1000);
+    driver.findElement(By.id("8")).click();
+	Thread.sleep(2000);
+	assertEquals("Tie!", driver.findElement(By.id("gameInfo")).getAttribute("innerHTML"));
     /* ... finish test! */
   }
 }
